a7729948da6cafdc4e86b3680f085796f4a078bd
Frieder Heugel <frieder.heugel@gmail.com>
2018-08-05 00:53:40 +0200

    Added missing resource files Fixed constant cots of checks Changed plugin key Updated readme Added missing dependency in test project

---
+++ test-project/src/main/java/test/vavr/VavrInterface.java
@@ -0,0 +1,20 @@
+package test.vavr;
+
+import io.vavr.control.Option;
+
+import java.util.List;
+import java.util.Map;
+
+public interface VavrInterface {
+
+    void func1(io.vavr.collection.List<Object> list);
+
+    void func2(io.vavr.collection.Map<String, Object> map);
+
+    void func3(List<Object> list);
+
+    void func4(Map<String, Object> map);
+
+    Option<String> func5();
+
+}
---
+++ test-project/src/main/java/test/vavr/VavrRegularClass.java
@@ -0,0 +1,91 @@
+package test.vavr;
+
+
+import io.vavr.control.Option;
+
+import java.util.List;
+import java.util.Map;
+
+public class VavrRegularClass {
+
+    public void func1(io.vavr.collection.List<Object> list) {
+
+    }
+
+    public void func2(io.vavr.collection.Map<String, Object> map) {
+
+    }
+
+    public void func3(List<Object> list) {
+
+    }
+
+    public void func4(Map<String, Object> map) {
+
+    }
+
+    public Option<String> func5() {
+        return null;
+    }
+
+    protected void func6(io.vavr.collection.List<Object> list) {
+
+    }
+
+    protected void func7(io.vavr.collection.Map<String, Object> map) {
+
+    }
+
+    protected void func8(List<Object> list) {
+
+    }
+
+    protected void func9(Map<String, Object> map) {
+
+    }
+
+    protected Option<String> func10() {
+        return null;
+    }
+
+    private void func11(io.vavr.collection.List<Object> list) {
+
+    }
+
+    private void func12(io.vavr.collection.Map<String, Object> map) {
+
+    }
+
+    private void func13(List<Object> list) {
+
+    }
+
+    private void func14(Map<String, Object> map) {
+
+    }
+
+    private Option<String> func15() {
+        return null;
+    }
+
+    void func16(io.vavr.collection.List<Object> list) {
+
+    }
+
+    void func17(io.vavr.collection.Map<String, Object> map) {
+
+    }
+
+    void func18(List<Object> list) {
+
+    }
+
+    void func19(Map<String, Object> map) {
+
+    }
+
+    Option<String> func20() {
+        return null;
+    }
+
+}
